3c1f36eda089406b72fb88b9911ab40e0c08d3e9
Mohamed <mguernaoui03@gmail.com>
2025-06-07 15:29:32 +0100

    Add custom email validator and update auth configuration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Validator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,8 +18,15 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot()
     {
-        //
+        Validator::extend("validateEmail", function (
+            $attribute,
+            $value,
+            $parameters,
+            $validator
+        ) {
+            return /* Your validation logic */;
+        });
     }
 }
