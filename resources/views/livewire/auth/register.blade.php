712049ac76ea11e8e2021645f595b7240f3c3fed
Mohamed <mguernaoui03@gmail.com>
2025-06-07 15:27:10 +0100

    Update User model namespace and normalize quotes

--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Models\User;
+use App\Modules\GestionUtilisateurs\Models\User;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
@@ -8,11 +8,11 @@ use Illuminate\Validation\Rules;
 use Livewire\Attributes\Layout;
 use Livewire\Volt\Component;
 
-new #[Layout('components.layouts.auth')] class extends Component {
-    public string $name = '';
-    public string $email = '';
-    public string $password = '';
-    public string $password_confirmation = '';
+new #[Layout("components.layouts.auth")] class extends Component {
+    public string $name = "";
+    public string $email = "";
+    public string $password = "";
+    public string $password_confirmation = "";
 
     /**
      * Handle an incoming registration request.
@@ -20,20 +20,36 @@ new #[Layout('components.layouts.auth')] class extends Component {
     public function register(): void
     {
         $validated = $this->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
-            'password' => ['required', 'string', 'confirmed', Rules\Password::defaults()],
+            "name" => ["required", "string", "max:255"],
+            "email" => [
+                "required",
+                "string",
+                "lowercase",
+                "email",
+                "max:255",
+                "unique:" . User::class,
+            ],
+            "password" => [
+                "required",
+                "string",
+                "confirmed",
+                Rules\Password::defaults(),
+            ],
         ]);
 
-        $validated['password'] = Hash::make($validated['password']);
+        $validated["password"] = Hash::make($validated["password"]);
 
         event(new Registered(($user = User::create($validated))));
 
         Auth::login($user);
 
-        $this->redirectIntended(route('dashboard', absolute: false), navigate: true);
+        $this->redirectIntended(
+            route("dashboard", absolute: false),
+            navigate: true
+        );
     }
-}; ?>
+};
+?>
 
 <div class="flex flex-col gap-6">
     <x-auth-header :title="__('Create an account')" :description="__('Enter your details below to create your account')" />
