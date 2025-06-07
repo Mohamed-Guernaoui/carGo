1cac8741bf771b16a12648a41374e0d3de6af710
Mohamed <mguernaoui03@gmail.com>
2025-06-08 00:19:38 +0100

    Update 0001_01_01_000000_create_users_table.php

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration {
             $table->string("name");
             $table->string("email")->unique();
             $table->timestamp("email_verified_at")->nullable();
-            $table->timestamp("cin")->unique();
+            $table->string("cin")->unique();
             $table->string("password");
             $table->enum("role", ["Proprietaire", "client"]);
             $table->rememberToken();
