503457c0594c492526307eb24457e910369de395
Mohamed <looklike882@gmail.com>
2025-06-08 22:24:39 +0100

    Add user role management and vehicle relationships
    This adds role-based methods, scopes for filtering users by role, and
    establishes vehicle ownership relationships. Also expands fillable fields.

--- app/Modules/GestionUtilisateurs/Models/User.php
+++ app/Modules/GestionUtilisateurs/Models/User.php
@@ -18,7 +18,15 @@ class User extends Authenticatable
      *
      * @var list<string>
      */
-    protected $fillable = ["name", "email", "password", "cin"];
+    protected $fillable = [
+        "name",
+        "email",
+        "password",
+        "cin",
+        "company_name",
+        "telephone",
+        "address",
+    ];
 
     /**
      * The attributes that should be hidden for serialization.
@@ -38,6 +46,42 @@ class User extends Authenticatable
             "email_verified_at" => "datetime",
             "password" => "hashed",
         ];
+    }
+
+    /**
+     * Scope a query to only include clients.
+     */
+    public function scopeClients($query)
+    {
+        return $query->where("role", "client");
+    }
+
+    /**
+     * Scope a query to only include vehicle owners.
+     */
+    public function scopeOwners($query)
+    {
+        return $query->where("role", "owner");
+    }
+
+    public function isClient(): bool
+    {
+        return $this->role === "client";
+    }
+
+    public function vehicles()
+    {
+        return $this->hasMany(
+            \App\Modules\GestionVehicules\Models\Vehicule::class,
+            "owner_id"
+        );
+    }
+    /**
+     * Check if user is a vehicle owner.
+     */
+    public function isOwner(): bool
+    {
+        return $this->role === "owner";
     }
 
     /**
